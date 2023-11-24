8abc78ce34e84479e75cc32869769f4effd96c3e
LukasReil <lukas_reil0305@gmx.net>
2023-11-24 19:32:26 +0100

    Project creation

---
+++ src/main/java/de/positronator/realisticbrewery/RealisticBrewery.java
@@ -0,0 +1,13 @@
+package de.positronator.realisticbrewery;
+
+import net.fabricmc.api.ModInitializer;
+
+public class RealisticBrewery implements ModInitializer {
+    /**
+     * Runs the mod initializer.
+     */
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/de/positronator/realisticbrewery/client/RealisticBreweryClient.java
@@ -0,0 +1,13 @@
+package de.positronator.realisticbrewery.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RealisticBreweryClient implements ClientModInitializer {
+    /**
+     * Runs the mod initializer on the client environment.
+     */
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
