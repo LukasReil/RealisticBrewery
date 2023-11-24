b30541d5a4b2cd2f520dca3816fb03476e12cc09
LukasReil <lukas_reil0305@gmx.net>
2023-11-24 20:30:08 +0100

    Project creation

--- src/main/java/de/positronator/realisticbrewery/RealisticBrewery.java
+++ src/main/java/de/positronator/realisticbrewery/RealisticBrewery.java
@@ -1,13 +1,18 @@
 package de.positronator.realisticbrewery;
 
+import de.positronator.realisticbrewery.modItems.ModItems;
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.server.MinecraftServer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class RealisticBrewery implements ModInitializer {
-    /**
-     * Runs the mod initializer.
-     */
+
+    public static final String MOD_ID = "realisticbrewery";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
     @Override
     public void onInitialize() {
-
+        ModItems.registerModItems();
     }
 }
---
+++ src/main/java/de/positronator/realisticbrewery/modItems/ModItems.java
@@ -0,0 +1,19 @@
+package de.positronator.realisticbrewery.modItems;
+
+import de.positronator.realisticbrewery.RealisticBrewery;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registry;
+
+
+public class ModItems {
+
+    private static Item registerItem(String itemName, Item item){
+        //return Registry.register(Registry.ITEM, new Indentifier(RealisticBrewery.MOD_ID, itemName), item);
+        return null;
+    }
+
+    public static void registerModItems(){
+
+    }
+
+}
